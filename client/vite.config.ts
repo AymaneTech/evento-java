19c19eed7e40e8f69530401bb970ff4fdc1d2c69
Aymane El Maini <elmainiaymane03@gmail.com>
2025-03-22 00:31:04 +0000

    configure tailwind and shadcn and also clean up the structure

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,37 +1,24 @@
-import path from "path";
-import AutoImport from "unplugin-auto-import/vite";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
-import Fonts from "unplugin-fonts/vite";
-// @ts-ignore
-import imagemin from "unplugin-imagemin/vite";
-import { compression } from "vite-plugin-compression2";
-import Inspect from "vite-plugin-inspect";
-import svgr from "vite-plugin-svgr";
-
-import { fonts } from "./configs/fonts.config";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [
-    svgr(),
-    react(),
-    Inspect(),
-    compression(),
-    imagemin(),
-    tailwindcss(),
-    Fonts({ google: { families: fonts } }),
-    AutoImport({
-      imports: ["react", "react-router"],
-      dts: "./auto-imports.d.ts",
-      eslintrc: { filepath: "./eslint.config.js" },
-      dirs: ["./src/components/ui"],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
 });
+
