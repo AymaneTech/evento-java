72bcbfe16c31d340e0b451a5311f4471075f1a3f
Aymane El Maini <elmainiaymane03@gmail.com>
2025-03-25 16:06:27 +0000

    fix liquibase error: missing column

--- server/src/main/resources/db/changelog/migrations/db.changelog-3.0.0.sql
+++ server/src/main/resources/db/changelog/migrations/db.changelog-3.0.0.sql
@@ -2,12 +2,14 @@ CREATE TYPE booking_status AS ENUM ('PENDING', 'APPROVED', 'REJECTED', 'CANCELLE
 
 CREATE TABLE bookings
 (
-    id             BIGSERIAL PRIMARY KEY NOT NULL,
-    booking_status booking_status        NOT NULL,
-    user_id        BIGSERIAL             NOT NULL,
-    event_id       BIGSERIAL             NOT NULL,
-    created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id              BIGSERIAL PRIMARY KEY NOT NULL,
+    status  booking_status        NOT NULL,
+    user_id         BIGSERIAL             NOT NULL,
+    event_id        BIGSERIAL             NOT NULL,
+    number_of_tickets INTEGER               NOT NULL,
+    total_price     DECIMAL               NOT NULL,
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_booking_event FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE,
     CONSTRAINT fk_booking_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
